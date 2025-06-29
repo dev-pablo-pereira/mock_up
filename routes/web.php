ed4b1885bdba39a83256b69d4715a760476559ad
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-29 06:59:50 -0300

    feat: Add name to route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('/login', [AdminController::class, 'index'])->name('admin.login');
+    Route::get('/login', [AdminController::class, 'index'])->name('admin.login')->name('admin.login');
 });
