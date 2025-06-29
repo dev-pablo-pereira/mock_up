54588f615531d930771bfcc45e876d913a96e13d
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-29 14:26:11 -0300

    feat: Create route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\Hotel\HotelController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/login', [AdminController::class, 'index'])->name('admin.login')->name('admin.login');
     Route::get('/present', [AdminController::class, 'present'])->name('admin.present');
+    Route::get('/new_hotel', [HotelController::class, 'new'])->name('admin.hotel.new');
 });
