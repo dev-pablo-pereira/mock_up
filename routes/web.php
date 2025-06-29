58405bcb029246b102c0a6fff42d62857e0f6675
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-29 10:04:58 -0300

    create route for present method of admin controller

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/login', [AdminController::class, 'index'])->name('admin.login')->name('admin.login');
+    Route::get('/present', [AdminController::class, 'present'])->name('admin.present');
 });
