0d660cfb6ea7bae416c1bce238ea05a102e01158
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-07-01 00:44:29 -0300

    feat: Add create method

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\Admin\Hotel\HotelController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('/login', [AdminController::class, 'index'])->name('admin.login')->name('admin.login');
+    Route::get('/login', [AdminController::class, 'index'])->name('admin.login');
     Route::get('/present', [AdminController::class, 'present'])->name('admin.present');
+    Route::get('/create', [AdminController::class, 'create'])->name('admin.create');
+    Route::post('/store', [AdminController::class, 'store'])->name('admin.store');
+
     Route::get('/hotel', [HotelController::class, 'index'])->name('admin.hotel.index');
     Route::get('/new_hotel', [HotelController::class, 'new'])->name('admin.hotel.new');
     Route::post('/create_hotel', [HotelController::class, 'create'])->name('admin.hotel.create');
