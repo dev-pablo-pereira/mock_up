0a0f0bcf008a4df13d477c6d48cb4a3f7fecd6e5
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-07-01 00:44:46 -0300

    feate: Add create method

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+
+    public function create() {
+        return view('admin.new');
+    }
+
+    public function store(Request $request) {
+
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            ]
+        );
+        User::create($data);
+        return redirect()->route('admin.login');
+    }
+
     public function index()
     {
         return view('admin.login');
